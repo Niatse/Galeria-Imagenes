d98d69edab8db267e5801491f077f539a42dcb04
Niatse <gameerseba@gmail.com>
2022-09-23 16:04:03 -0300

    fixtry

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,16 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
 
-const handler = async ( req: NextApiRequest,
-  res: NextApiResponse) => {
-  await res.revalidate("/");
-
-  const pathToRevalidate = `/${
-    req.body?.record?.id || req.body?.old_record?.id
-  }`;
-  await res.revalidate(pathToRevalidate);
-
-  return res.send({ revalidated: true });
-};
-
-export default handler;
+  try {
+    // Regenerate our index route showing the images
+    await fetch('/');
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating')
+  }
+}
